9a41e138692d903c60ea223fb318a4d1d5361193
root <root@localhost.localdomain>
2025-02-07 13:30:39 +0900

    2025-02-07 fix menu

--- db/maindb.php
+++ db/maindb.php
@@ -1,9 +1,9 @@
 <?php
 
 $host = 'localhost';
-// $servername = "wargame-db-service"; // 쿠버네티스 Service 이름
+// $servername = "db-service"; // 쿠버네티스 Service 이름
 $db = 'LED_WG';
-$user = 'wargame';
+$user = 'root';
 $pass = '1234';
 // MySQL 연결 생성
 // $conn = new mysqli($servername, $user, $pass, $db);
